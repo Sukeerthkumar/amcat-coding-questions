ce0064b542b83b7d85f7f71be219d9baa582525e
Swarup <swaruphegde@gmail.com>
2019-10-02 16:37:56 +0530

    program to check if number is automorphic

--- automorphic.py
+++ automorphic.py
@@ -1,17 +1,23 @@
-# Program to find if number is automorphic or not
+# Program to check if a given number is automorphic or not
 
-def automorphic_check(x):
-    squared = str(int(x) ** 2)
-    diff = len(squared) - len(x)
-    squared = squared[diff:]
-    if squared == x:
+def automorphic_check(number):
+    squared = str(int(number)**2)
+    r_squared = squared[::-1]
+    r_check = []
+    i = len(number)
+    for x in r_squared:
+        if i == 0:
+            break
+        r_check.append(x)
+        i -= 1
+    check_number = ''.join(r_check[::-1])
+    if check_number == number:
         return True
     else:
         return False
 
 number = input('Enter any number: ')
 if automorphic_check(number):
-    print('Entered number is an automorphic number')
+    print(number + ' is an automorphic number')
 else:
-    print('Entered number is not an automorphic number')
-
+    print(number + ' is not an automorphic number')
